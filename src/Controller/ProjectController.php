cc98ab1cd6a8b65fec6383f056afbaf6bce60936
prosumers <zouarihaytham@outlook.com>
2020-03-24 11:41:42 +0000

    traitement postes horaires corrigé

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -174,8 +174,9 @@ class ProjectController extends AbstractController
     public function show(Project $project,Request $request,Session $session)
     {
         date_default_timezone_set("Africa/Tunis");
-        if ($request->isXmlHttpRequest()){
-            $consomation = new Consomation();
+    if ($request->isXmlHttpRequest()){
+        
+            $consomation =$project->getConsomation();
             $dateDeb= new \DateTime($request->get('dateDeb'));
             $dateDebexel= new \DateTime();
             $dateDebexel->setTimestamp($session->get('dateDebExl'));
@@ -184,7 +185,7 @@ class ProjectController extends AbstractController
             $dateFin= new \DateTime($request->get('dateDeb'));
             $dateFin->add(new \DateInterval('P1Y'));
             $avgweek=$project->getConsomation()->getAvgweek();
-           
+            $dataexl = $session->get('data');
             $activite=$request->get('activite');
             $dimancheCheck=$request->get('dimancheCheck');
             $hourSlider_dimanche=$request->get('hourSlider_dimanche');
@@ -201,7 +202,7 @@ class ProjectController extends AbstractController
             $dateconge_fin= new \DateTime($request->get('dateconge_fin'));
             
             
-            if (empty($avgweek[0][0][0])){
+        if (empty($avgweek[0][0][0])){
             
             $hourSliderreq1=explode(',',$hourSlider1);
             $hourSliderreq2=explode(',',$hourSlider2);
@@ -225,7 +226,7 @@ class ProjectController extends AbstractController
             }
 
 
-            $Phd=1;$Phf=1;$Phm=20;$Phad=12;$Pham=12;$Phn=0;
+            $Phd=1;$Phf=1;$Phm=1;$Phad=1;$Pham=1;$Phn=1;
 
             $hd1=(int)$hourSliderreq1[0];$hf1=(int)$hourSliderreq1[1];$hm1=round(($hd1+$hf1)/2);$had1=round(($hd1+$hm1)/2);$ham1=round(($hf1+$hm1)/2);
             $hd2=(int)$hourSliderreq2[0];$hf2=(int)$hourSliderreq2[1];$hm2=round(($hd2+$hf2)/2);$had2=round(($hd2+$hm2)/2);$ham2=round(($hf2+$hm2)/2);
@@ -601,7 +602,7 @@ class ProjectController extends AbstractController
                             }
                         }    
                     }
-
+                    if($request->get('tarif')==0){
                     $monthp = $request->get('date');
                     $month=[];
                     for($i=0;$i<13;$i++){
@@ -642,16 +643,82 @@ class ProjectController extends AbstractController
                     $consomation->setCmMonth(PostHoraire::PostHoraire($dataSynW));
                         $dataAvgW=$dataSynW;
                         $dataAvgWtm=$dataSynWtm;
+                    }
+                        else {
+                            $dataSynWtm=$dataSynW;
+                            $monthp = $request->get('datehoraire');
+                            //$month is an array that contain the sum of the hours of every type in every month
+                            $month=[];
+                            for($i=0;$i<13;$i++){
+                                $month[$i]['jour']=0;
+                                $month[$i]['nuit']=0;
+                                $month[$i]['soir']=0;
+                                $month[$i]['ete']=0;
+                            }
+                            $month = PostHoraire::PostHoraire($dataSynW);
+                            $monthtmp = $month;
+                            $consomation->setCmMonth($month);
+                            for($i =1 ;$i < 13 ; $i++){
+                                if($i>5&&$i<9){
+                                    $month[$i]['jour']=$monthp['jour'][$i]/$month[$i]['jour'];
+                                    $month[$i]['nuit']=$monthp['nuit'][$i]/$month[$i]['nuit'];
+                                    $month[$i]['soir']=$monthp['soir'][$i]/$month[$i]['soir'];
+                                    $month[$i]['ete']=$monthp['ete'][$i]/$month[$i]['ete'];
+                                }
+                                else{
+                                    $month[$i]['jour']=$monthp['jour'][$i]/$month[$i]['jour'];
+                                    $month[$i]['nuit']=$monthp['nuit'][$i]/$month[$i]['nuit'];
+                                    $month[$i]['soir']=$monthp['soir'][$i]/$month[$i]['soir'];
+                                }
+                            }
+                            for($i=0;$i<count($dataSynW);$i++) {
+                                $date = new \DateTime();
+                                $date->setTimestamp($dataSynW[$i][0]);
+                                if (((int)$date->format('m')) > 5 && ((int)$date->format('m')) < 9) {
+                                    if( (int)$date->format('H')>6&&(int)$date->format('H')<9 ||
+                                        (int)$date->format('H')>13&&(int)$date->format('H')<19){
+                                        $dataSynW[$i][1]*=$month[((int)$date->format('m'))]['jour'];
+                                    }
+                                    elseif ((int)$date->format('H')>=0&&(int)$date->format('H')<=6 ||
+                                        (int)$date->format('H')>21&&(int)$date->format('H')<=23){
+                                        $dataSynW[$i][1]*=$month[((int)$date->format('m'))]['nuit'];
+                                    }
+                                    elseif ((int)$date->format('H')>=9&&(int)$date->format('H')<14){
+                                        $dataSynW[$i][1]*=$month[((int)$date->format('m'))]['ete'];
+                                    }
+                                    else
+                                        $dataSynW[$i][1]*=$month[((int)$date->format('m'))]['soir'];
+                                }
+                                else{
+                                    if( (int)$date->format('H')>6&&(int)$date->format('H')<18){
+                                        $dataSynW[$i][1]*=$month[((int)$date->format('m'))]['jour'];
+                                    }
+                                    elseif ((int)$date->format('H')>=0&&(int)$date->format('H')<=6 ||
+                                        (int)$date->format('H')>=21&&(int)$date->format('H')<=23){
+                                        $dataSynW[$i][1]*=$month[((int)$date->format('m'))]['nuit'];
+                                    }
+                                    else {
+                                        $dataSynW[$i][1]*=$month[((int)$date->format('m'))]['soir'];
+                                    }
+                                }
+                            }
+
+                            $dataAvgW=$dataSynW;
+                            $dataAvgWtm=$dataSynWtm;
+                           
+                        }
+            
 
                     //******************************************************* 
 
                 
-                    }
-            else {            
-            $dataAvgW=[];
-            $i =0;
-            if ($session->get('avgweek')[0][0][0]===$avgweek[0][0][0]){
-                $dataexl = $session->get('data');
+        }
+            else {     
+                       
+                $dataAvgW=[];
+                $i =0;
+            
+                
                 if(((int)$dateDeb->format('m') )>5 && ((int)$dateDeb->format('m'))<9) {
                     $dataAvgW[] = [$dateDeb->getTimestamp(),$avgweek[0][((int)$dateDeb->format('w'))][((int)$dateDeb->format('H'))]];
                 }
@@ -714,7 +781,6 @@ class ProjectController extends AbstractController
                     $month=[];
                     for($i=0;$i<13;$i++){
                         $month[$i]=0;
-
                     }
                     foreach ($dataAvgW as $item) {
                         $date = new \DateTime();
@@ -727,7 +793,7 @@ class ProjectController extends AbstractController
                             $month[$i]=0;
                         }
                         else{
-                        $month[$i]=(float)$monthp[$i]/$month[$i];
+                            $month[$i]=(float)$monthp[$i]/$month[$i];
                         }
                     }
                     $dataAvgWtm=$dataAvgW;
@@ -765,6 +831,7 @@ class ProjectController extends AbstractController
                         $month[$i]['soir']=0;
                         $month[$i]['ete']=0;
                     }
+                    $dataAvgWtm=$dataAvgW;
                     $month = PostHoraire::PostHoraire($dataAvgW);
                     $monthtmp = $month;
                     $consomation->setCmMonth($month);
@@ -814,159 +881,8 @@ class ProjectController extends AbstractController
                     }
                 }
 
-            }
-            else {
-                if(((int)$dateDeb->format('m') )>5 && ((int)$dateDeb->format('m'))<9) {
-                    $dataAvgW[] = [$dateDeb->getTimestamp(),$avgweek[0][((int)$dateDeb->format('w'))][((int)$dateDeb->format('H'))]];
-                }
-                else
-                    $dataAvgW[] = [$dateDeb->getTimestamp(),$avgweek[1][((int)$dateDeb->format('w'))][((int)$dateDeb->format('H'))]];
-                $period = CarbonPeriod::create($dateDeb->format('y-m-d'),'PT1H',$dateFin->format('y-m-d') , CarbonPeriod::EXCLUDE_START_DATE);
-                //creating a period to loop on it
-                foreach ($period as $key=>$date) {
-                    if(((int)$date->format('m') )>5 && ((int)$date->format('m'))<9) {
-                        $dataAvgW[] = [$date->getTimestamp(),$avgweek[0][((int)$date->format('w'))][((int)$date->format('H'))]];
-                    }
-                    else{
-                        $dataAvgW[] = [$date->getTimestamp(),$avgweek[1][((int)$date->format('w'))][((int)$date->format('H'))]];
-                    }
-
-                }
-                $dataAvgWtm=$dataAvgW;
-                array_pop($dataAvgW);
-
-                for ($i=0;$i<count($dataAvgW);$i++) {
-                    $date = new \DateTime();
-                    $date->setTimestamp($dataAvgW[$i][0]);
-                    if(((int)$date->format('d'))==1&&((int)$date->format('m'))==1){
-                        $dataAvgW[$i][1]=$avgweek[1][0][((int)$date->format('H'))];
-                    }
-                    elseif(((int)$date->format('d'))==14&&((int)$date->format('m'))==1){
-                        $dataAvgW[$i][1]=$avgweek[1][0][((int)$date->format('H'))];
-                    }
-                    elseif(((int)$date->format('d'))==20&&((int)$date->format('m'))==3){
-                        $dataAvgW[$i][1]=$avgweek[1][0][((int)$date->format('H'))];
-                    }
-                    elseif(((int)$date->format('d'))==9&&((int)$date->format('m'))==4){
-                        $dataAvgW[$i][1]=$avgweek[1][0][((int)$date->format('H'))];
-                    }
-                    elseif(((int)$date->format('d'))==1&&((int)$date->format('m'))==5){
-                        $dataAvgW[$i][1]=$avgweek[1][0][((int)$date->format('H'))];
-                    }
-                    elseif(((int)$date->format('d'))==25&&((int)$date->format('m'))==7){
-                        $dataAvgW[$i][1]=$avgweek[0][0][((int)$date->format('H'))];
-                    }
-                    elseif(((int)$date->format('d'))==13&&((int)$date->format('m'))==8){
-                        $dataAvgW[$i][1]=$avgweek[0][0][((int)$date->format('H'))];
-                    }
-                    elseif(((int)$date->format('d'))==15&&((int)$date->format('m'))==10){
-                        $dataAvgW[$i][1]=$avgweek[1][0][((int)$date->format('H'))];
-                    }
-                }
-                //delete a hole year
-               /* $dataAvgW[count($dataAvgW)-1][0]=$dataAvgW[count($dataAvgW)-1][0]-31536000;*/
-                if($request->get('tarif')==0){
-                    $monthp = $request->get('date');
-                    $month=[];
-                    for($i=0;$i<13;$i++){
-                        $month[$i]=0;
-
-                    }
-                    foreach ($dataAvgW as $item) {
-                        $date = new \DateTime();
-                        $date->setTimestamp($item[0]);
-                        $month[((int)$date->format('m'))]+=$item[1];
-                    }
-                    $monthtmp=$month;
-                    for($i =1 ; $i <13; $i++){
-                        $month[$i]=(float)$monthp[$i]/$month[$i];
-                    }
-
-                    $dataAvgWtm=$dataAvgW;
-                    for ($i=0;$i<count($dataAvgW);$i++) {
-                        $date = new \DateTime();
-                        $date->setTimestamp($dataAvgW[$i][0]);
-                        $months=(int)$date->format('m');
-                        $day=(int)$date->format('d');
-                        $dataAvgW[$i][1]*=$month[(int)((new \DateTime())->setTimestamp($dataAvgW[$i][0])->format('m'))];
-                    }
-                    $month=[];
-                    for($i=0;$i<13;$i++){
-                        $month[$i]['jour']=0;
-                        $month[$i]['ete']=0;
-                        $month[$i]['soir']=0;
-                        $month[$i]['nuit']=0;
-
-                    }
-                    $consomation->setCmMonth(PostHoraire::PostHoraire($dataAvgW));
-
-
-                }
-                //*********************************************************************************************************
-                //*********************************************************************************************************
-                //*********************************************************************************************************
-                //*********************************************************************************************************
-                //*********************************************************************************************************
-                else {
-                    $monthp = $request->get('datehoraire');
-                    //$month is an array that contain the sum of the hours of every type in every month
-                    $month=[];
-                    for($i=0;$i<13;$i++){
-                        $month[$i]['jour']=0;
-                        $month[$i]['nuit']=0;
-                        $month[$i]['soir']=0;
-                        $month[$i]['ete']=0;
-                    }
-                    $month = PostHoraire::PostHoraire($dataAvgW);
-                    $monthtmp = $month;
-                    $consomation->setCmMonth($month);
-                    for($i =1 ;$i < 13 ; $i++){
-                        if($i>5&&$i<9){
-                            $month[$i]['jour']=$monthp['jour'][$i]/$month[$i]['jour'];
-                            $month[$i]['nuit']=$monthp['nuit'][$i]/$month[$i]['nuit'];
-                            $month[$i]['soir']=$monthp['soir'][$i]/$month[$i]['soir'];
-                            $month[$i]['ete']=$monthp['ete'][$i]/$month[$i]['ete'];
-                        }
-                        else{
-                            $month[$i]['jour']=$monthp['jour'][$i]/$month[$i]['jour'];
-                            $month[$i]['nuit']=$monthp['nuit'][$i]/$month[$i]['nuit'];
-                            $month[$i]['soir']=$monthp['soir'][$i]/$month[$i]['soir'];
-                        }
-                    }
-                    for($i=0;$i<count($dataAvgW);$i++) {
-                        $date = new \DateTime();
-                        $date->setTimestamp($dataAvgW[$i][0]);
-                        if (((int)$date->format('m')) > 5 && ((int)$date->format('m')) < 9) {
-                            if( (int)$date->format('H')>6&&(int)$date->format('H')<9 ||
-                                (int)$date->format('H')>13&&(int)$date->format('H')<19){
-                                $dataAvgW[$i][1]*=$month[((int)$date->format('m'))]['jour'];
-                            }
-                            elseif ((int)$date->format('H')>=0&&(int)$date->format('H')<=6 ||
-                                (int)$date->format('H')>21&&(int)$date->format('H')<=23){
-                                $dataAvgW[$i][1]*=$month[((int)$date->format('m'))]['nuit'];
-                            }
-                            elseif ((int)$date->format('H')>=9&&(int)$date->format('H')<14){
-                                $dataAvgW[$i][1]*=$month[((int)$date->format('m'))]['ete'];
-                            }
-                            else
-                                $dataAvgW[$i][1]*=$month[((int)$date->format('m'))]['soir'];
-                        }
-                        else{
-                            if( (int)$date->format('H')>6&&(int)$date->format('H')<18){
-                                $dataAvgW[$i][1]*=$month[((int)$date->format('m'))]['jour'];
-                            }
-                            elseif ((int)$date->format('H')>=0&&(int)$date->format('H')<=6 ||
-                                (int)$date->format('H')>=21&&(int)$date->format('H')<=23){
-                                $dataAvgW[$i][1]*=$month[((int)$date->format('m'))]['nuit'];
-                            }
-                            else {
-                                $dataAvgW[$i][1]*=$month[((int)$date->format('m'))]['soir'];
-                            }
-                        }
-                    }
-                }
-
-            }
+           
+       
         }
             $consomation->setDateDeb($dateDeb);
             $consomation->setDateFin($dateFin);
@@ -979,7 +895,7 @@ class ProjectController extends AbstractController
             $entityManager->persist($project);
             $entityManager->flush();
             return new JsonResponse(['virgdata'=>$dataAvgW,'data'=>$dataAvgWtm,'data2'=>$monthtmp]);
-        }
+    }
         return $this->render('project/show.html.twig', [
             'project' => $project,
         ]);
--- src/Service/PostHoraire.php
+++ src/Service/PostHoraire.php
@@ -18,14 +18,42 @@ class PostHoraire
         foreach ($data as $item) {
             $date = new \DateTime();
             $date->setTimestamp($item[0]);
-            if (((int)$date->format('m')) > 5 && ((int)$date->format('m')) < 9) {
-                if( (int)$date->format('H')>=7&&(int)$date->format('H')<=8 ||
-                    (int)$date->format('H')>13&&(int)$date->format('H')<=18){
-                    $month[((int)$date->format('m'))]['jour']+=$item[1];
-                }
-                elseif ((int)$date->format('H')>=0&&(int)$date->format('H')<=6 ||
+            if(((int)$date->format('w'))==0)
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            elseif(((int)$date->format('d'))==1&&((int)$date->format('m'))==1){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+            elseif(((int)$date->format('d'))==14&&((int)$date->format('m'))==1){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+            elseif(((int)$date->format('d'))==20&&((int)$date->format('m'))==3){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+            elseif(((int)$date->format('d'))==9&&((int)$date->format('m'))==4){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+            elseif(((int)$date->format('d'))==1&&((int)$date->format('m'))==5){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+            elseif(((int)$date->format('d'))==25&&((int)$date->format('m'))==7){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+            elseif(((int)$date->format('d'))==13&&((int)$date->format('m'))==8){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+            elseif(((int)$date->format('d'))==15&&((int)$date->format('m'))==10){
+                $month[((int)$date->format('m'))]['nuit']+=$item[1];
+            }
+
+            elseif (((int)$date->format('m')) > 5 && ((int)$date->format('m')) < 9) {
+                
+                if ((int)$date->format('H')>=0&&(int)$date->format('H')<=6 ||
                     (int)$date->format('H')>21&&(int)$date->format('H')<=23){
                     $month[((int)$date->format('m'))]['nuit']+=$item[1];
+                }
+                elseif( (int)$date->format('H')>=7&&(int)$date->format('H')<=8 ||
+                    (int)$date->format('H')>13&&(int)$date->format('H')<=18){
+                    $month[((int)$date->format('m'))]['jour']+=$item[1];
                 }
                 elseif ((int)$date->format('H')>=9&&(int)$date->format('H')<=13){
                     $month[((int)$date->format('m'))]['ete']+=$item[1];
